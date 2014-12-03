495b8954706cc3659073faa67a55a6fed50c23fe
BilboBeutlin <hen3.pohl@gmail.com>
2014-12-04 00:30:13 +0100

    Klasse decode hinzugefügt

--- decode_aprs_short/decode_aprs_short/MainWindow.xaml.cs
+++ decode_aprs_short/decode_aprs_short/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace decode_aprs_short
 {
@@ -23,6 +24,42 @@ namespace decode_aprs_short
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                string text = System.IO.File.ReadAllText(dialog.FileName); // Liest die *.txt ein
+
+               string[] parts = text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries); // Die *.txt wird zeilen weise in ein Array geschrieben
+               List<decode> encode = new List<decode>();
+               for (int i = 0; i < parts.Length; i++)
+               {
+                   if (parts[i] != "")
+                   {
+                       bool exist = false;
+                       for (int j = 0; j < encode.Count; j++)
+                       {
+                           if (encode[j].GetText() == parts[i] )
+                           {
+                               // TODO: PlusOne() ist nicht nötig
+                                encode[j].PlusOne();
+                                exist = true;
+                                break;
+                           }                          
+                       }
+                       if (!exist)
+                       {
+                           encode.Add(new decode(parts[i]));
+                       }
+                   }
+               }
+           
+            }
         }
     }
 }
