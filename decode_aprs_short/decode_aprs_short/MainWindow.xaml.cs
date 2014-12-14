1c43edadf5be654c1ec269f3510842ed44da630b
BilboBeutlin <hen3.pohl@gmail.com>
2014-12-14 19:35:13 +0100

    Datum_Zeit auslesen

--- decode_aprs_short/decode_aprs_short/MainWindow.xaml.cs
+++ decode_aprs_short/decode_aprs_short/MainWindow.xaml.cs
@@ -33,32 +33,22 @@ namespace decode_aprs_short
 
             if (result == true)
             {
-                string text = System.IO.File.ReadAllText(dialog.FileName); // Liest die *.txt ein
-
-               string[] parts = text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries); // Die *.txt wird zeilen weise in ein Array geschrieben
+               string [] new_data = System.IO.File.ReadAllLines(dialog.FileName);
                List<decode> encode = new List<decode>();
-               for (int i = 0; i < parts.Length; i++)
+               for (int i = 0; i < new_data.Length; i++)
                {
-                   if (parts[i] != "")
-                   {
-                       bool exist = false;
-                       for (int j = 0; j < encode.Count; j++)
-                       {
-                           if (encode[j].GetText() == parts[i] )
-                           {
-                               // TODO: PlusOne() ist nicht nötig
-                                encode[j].PlusOne();
-                                exist = true;
-                                break;
-                           }                          
-                       }
-                       if (!exist)
-                       {
-                           encode.Add(new decode(parts[i]));
-                       }
-                   }
-               }
-           
+                       encode.Add(new decode(new_data[i]));
+                   // Datum + Uhrzeit aus System lesen und in das entsprechende Format bringen
+                       DateTime sytem_date = System.DateTime.Now;                               // System Datum+Uhrzeit holen
+                       sytem_date = System.DateTime.SpecifyKind(sytem_date, DateTimeKind.Utc);  // System Datum+Uhrzeit  in UTC    
+                       string sytem_date_str = sytem_date.ToUniversalTime().ToString("u");      // System Datum+Uhrzeit Konvertieren in "Universelles, sortierbares Datums-/Zeitmuster."
+                       sytem_date_str = sytem_date_str.Remove(sytem_date_str.Length-1);         // Das letzte Zeichen "Z" aus dem Datum löschen, wird immer mit rangehängt
+                   // Comprimierte Elemente im Protokoll finden                                 // lala
+                        
+                    // Ausgabe in Text Box
+                       myTextBox.Text = sytem_date_str; 
+                   
+               }           
             }
         }
     }
